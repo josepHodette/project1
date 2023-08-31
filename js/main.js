68040384ed9d01ffd3e291326f1fff89c946cf74
josepHodette <lyrickor@gmail.com>
2023-08-31 14:21:35 +0900

    메뉴작업

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     $("#gnb>li").on("mousemove",function(){
         $(".submenu").stop().slideDown(500);
+        $(".menubg").stop().slideDown(500);
     });
     $("#gnb>li").on("mouseleave",function(){
         $(".submenu").stop().slideUp(500);
+        $(".menubg").stop().slideUp(500);
     });
 
 })
